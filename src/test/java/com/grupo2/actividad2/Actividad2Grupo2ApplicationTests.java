2219a89c925d19302c0eafb0e352e7fb40189274
kmorales-2025027 <kmorales-202527@kinal.edu.gt>
2026-01-30 20:34:42 -0600

    Commit inicial

---
+++ src/test/java/com/grupo2/actividad2/Actividad2Grupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.actividad2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad2Grupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
