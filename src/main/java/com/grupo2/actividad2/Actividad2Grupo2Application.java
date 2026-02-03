00f80a649f57f4ae258a17a82699070aa3a1d4b0
angel <amunoz-2025042@kinal.edu.gt>
2026-02-02 19:47:26 -0600

    Primer commit en feature/angel

---
+++ src/main/java/com/grupo2/actividad2/Actividad2Grupo2Application.java
@@ -0,0 +1,13 @@
+package com.grupo2.actividad2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actividad2Grupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actividad2Grupo2Application.class, args);
+	}
+
+}
